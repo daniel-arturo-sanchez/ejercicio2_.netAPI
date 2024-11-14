b9c6ab887d1ea3c2e8bdf682a7abaad6f982242b
2dawa <2dawa@informatica.edu>
2024-11-14 09:01:22 +0000

    Meh

--- Controllers/JuegosController.cs
+++ Controllers/JuegosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer.Localisation;
+using Microsoft.AspNetCore.Mvc;
 using WebApi1.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,6 +29,7 @@ namespace WebApi1.Controllers
         public static int juegoId = juegos.Count();
         // GET: api/<JuegosController>
         [HttpGet]
+        [ProducesResponseType(200)]
         public ActionResult Get()
         {
             return Ok(juegos);
@@ -35,8 +37,13 @@ namespace WebApi1.Controllers
 
         // GET api/<JuegosController>/5
         [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public ActionResult Get(int id)
         {
+            if (id == null)
+                return BadRequest();
             Juego juego = juegos.Find(x => x.Id == id);
             if (juego == null)
                 return NotFound();
@@ -45,6 +52,8 @@ namespace WebApi1.Controllers
 
         // POST api/<JuegosController>
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
         public void Post([FromBody] Juego juego)
         {
             juego.Id = ++juegoId;
@@ -60,14 +69,63 @@ namespace WebApi1.Controllers
 
         // PUT api/<JuegosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult Put(int id, string title, string genre)
         {
+            if (id == null || title == null || genre == null)
+            {
+                return BadRequest("Parámetros no válidos");
+            } 
+            else
+            {
+                Juego ToUpdateGame = juegos.Find(juego => juego.Id == id);
+                if (ToUpdateGame == null)
+                {
+                    return NotFound("No existe un juego con este índice");
+                }
+                else
+                {
+                    Juego juego = new Juego { Id = id, Title = title, Genre = genre };
+                    if (TryValidateModel(juego))
+                    {
+                        ToUpdateGame.Title = juego.Title;
+                        ToUpdateGame.Genre = juego.Genre;
+                    }
+                    else
+                    {
+                        return BadRequest("No cumples con las restricciones del modelo");
+                    }
+                }
+                return Ok("Actualizado");
+            }
         }
 
         // DELETE api/<JuegosController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult Delete(int id)
         {
+            if (id == null)
+            {
+                return BadRequest("Parámetros no válidos");
+            }
+            else
+            {
+                Juego ToUpdateGame = juegos.Find(juego => juego.Id == id);
+                if (ToUpdateGame == null)
+                {
+                    return NotFound("No existe un juego con este índice");
+                }
+                else
+                {
+                    juegos.Remove(ToUpdateGame);
+                }
+                return Ok("Juego removido");
+            }
         }
     }
 }
--- Models/Juego.cs
+++ Models/Juego.cs
@@ -9,12 +9,12 @@ namespace WebApi1.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Debes rellenar este campo")]
         [DisplayName("Título")]
-        [StringLength(15, ErrorMessage = "El título debe tener entre {2} y {0} caracteres",MinimumLength = 5)]
+        [StringLength(30, ErrorMessage = "El título debe tener entre {2} y {0} caracteres",MinimumLength = 5)]
 
         public string Title { get; set; }
         [Required]
         [DisplayName("Género")]
-        [StringLength(12, ErrorMessage = "El título debe tener entre {2} y {0} caracteres", MinimumLength = 2)]
+        [StringLength(25, ErrorMessage = "El título debe tener entre {2} y {0} caracteres", MinimumLength = 2)]
         public string Genre { get; set; }
     }
 }
